ca77c6839f78f1a674e348512fce559bffcfec9c
Balthild <ibalthild@gmail.com>
2026-02-19 15:32:12 -0600

    Write renderer jobs in theme class

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,14 +1,6 @@
-import * as fs from 'fs/promises';
-import * as path from 'path';
-import { promisify } from 'util';
-import { deflate } from 'zlib';
+import { Application, RendererEvent } from 'typedoc';
 
-import { Application, Reflection, RendererEvent } from 'typedoc';
-
-import { createSearchDocument } from '../lib';
-import { itemLink } from './context/utils';
 import { OxideTheme } from './theme';
-import { renderTextComment } from './utils';
 
 declare module 'typedoc' {
   namespace JSX.JSX {
@@ -30,55 +22,4 @@ declare module 'typedoc' {
  */
 export function load(app: Application) {
   app.renderer.defineTheme('oxide', OxideTheme);
-
-  app.renderer.preRenderAsyncJobs.push(async (event: RendererEvent) => {
-    const document = createSearchDocument();
-
-    app.renderer.router!.getLinkTargets()
-      .filter((x) => x instanceof Reflection)
-      .filter((x) => x.isDeclaration() || x.isDocument())
-      .filter((x) => x.name && !x.flags.isExternal)
-      .forEach((item) => {
-        try {
-          document.add({
-            id: item.id,
-            name: item.name,
-            comment: renderTextComment(item),
-            kind: item.kind,
-            parent: item.parent?.isProject() ? '' : item.parent?.getFriendlyFullName() ?? '',
-            url: itemLink(app.renderer.router!, item, false),
-          });
-        } catch {
-          // the target do not have a url
-        }
-      });
-
-    const parts: Record<string, any> = {};
-    document.export((key, data) => {
-      app.logger.info(`search index part: ${key}`);
-      parts[key] = JSON.parse(data);
-    });
-
-    const buffer = await promisify(deflate)(Buffer.from(JSON.stringify(parts)));
-
-    const dest = path.join(event.outputDirectory, 'assets', 'oxide');
-    await fs.mkdir(dest, { recursive: true });
-    await fs.writeFile(path.join(dest, 'search-index.deflate'), buffer);
-  });
-
-  app.renderer.postRenderAsyncJobs.push(async (event: RendererEvent) => {
-    const src = path.join(import.meta.dirname, '..', 'assets');
-    const dest = path.join(event.outputDirectory, 'assets', 'oxide');
-
-    try {
-      await fs.access(src, fs.constants.F_OK);
-      await fs.access(path.join(src, 'rustdoc'), fs.constants.F_OK);
-      await fs.mkdir(dest, { recursive: true });
-      await fs.cp(src, dest, { recursive: true });
-    } catch (e) {
-      console.log(e);
-      app.logger.error('some front-end assets are missing.');
-      app.logger.error('users of the theme should not see this, or I must have done something silly.');
-    }
-  });
 }
--- src/plugin/theme.ts
+++ src/plugin/theme.ts
@@ -1,9 +1,72 @@
-import { DefaultTheme, PageEvent, Reflection } from 'typedoc';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { promisify } from 'util';
+import { deflate } from 'zlib';
 
+import { DefaultTheme, PageEvent, Reflection, RendererEvent } from 'typedoc';
+
+import { createSearchDocument } from '../lib';
 import { OxideContext } from './context';
+import { itemLink } from './context/utils';
+import { renderTextComment } from './utils';
 
 export class OxideTheme extends DefaultTheme {
   getRenderContext(page: PageEvent<Reflection>) {
     return new OxideContext(this.router, this, page, this.application.options);
+  }
+
+  async preRender(event: RendererEvent) {
+    super.preRender(event);
+
+    const document = createSearchDocument();
+
+    this.application.renderer.router!.getLinkTargets()
+      .filter((x) => x instanceof Reflection)
+      .filter((x) => x.isDeclaration() || x.isDocument())
+      .filter((x) => x.name && !x.flags.isExternal)
+      .forEach((item) => {
+        try {
+          document.add({
+            id: item.id,
+            name: item.name,
+            comment: renderTextComment(item),
+            kind: item.kind,
+            parent: item.parent?.isProject() ? '' : item.parent?.getFriendlyFullName() ?? '',
+            url: itemLink(this.router!, item, false),
+          });
+        } catch {
+          // the target do not have a url
+        }
+      });
+
+    const parts: Record<string, any> = {};
+    document.export((key, data) => {
+      this.application.logger.info(`search index part: ${key}`);
+      parts[key] = JSON.parse(data);
+    });
+
+    const buffer = await promisify(deflate)(Buffer.from(JSON.stringify(parts)));
+
+    const dest = path.join(event.outputDirectory, 'assets', 'oxide');
+    await fs.mkdir(dest, { recursive: true });
+    await fs.writeFile(path.join(dest, 'search-index.deflate'), buffer);
+  }
+
+  async postRender(event: RendererEvent) {
+    super.postRender(event);
+
+    const src = path.join(import.meta.dirname, '..', 'assets');
+    const dest = path.join(event.outputDirectory, 'assets', 'oxide');
+
+    try {
+      await fs.access(src, fs.constants.F_OK);
+      await fs.access(path.join(src, 'rustdoc'), fs.constants.F_OK);
+      await fs.mkdir(dest, { recursive: true });
+      await fs.cp(src, dest, { recursive: true });
+    } catch (e) {
+      console.log(e);
+      this.application.logger.error('some front-end assets are missing.');
+      this.application.logger.error('users of the theme should not see this, or I must have done something silly.');
+    }
   }
 }
